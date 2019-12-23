bed588188295535bfec412735be0e76b7488750f
Nataliia Chaika <larusbiz@gmail.com>
2019-12-23 11:54:30 +0200

    Java Lesson 1 with an additional one

---
+++ JavaLesson1_2/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+
+import java.util.Scanner;
+
+    public class Main {
+
+        public static void main(String[] args) {
+            String a;
+            var Scanner= new Scanner(System.in);
+            System.out.println("Enter sigh:");
+            a= Scanner.nextLine();
+            System.out.println(a +"     "+ a+           "  "+a+" "+a+" "+a +"  "  +a+          "        "+a                        +"         "+a+" "+a );
+            System.out.println(a+"     "+  a+           "  "+a+           "      "+a       +   "        "+a                       +  "        "+a+"    "+a);
+            System.out.println(a+" "+ a+" "+a+" "+a+    "  "+a+" "+a       +"    "+a       +   "        "+a                         +"       "+a+"      "+a);
+            System.out.println(a+"     "+ a+            "  "+a+           "      "+a +         "     "+a  +"  "+a             +"     "+a +"  "+a+"    "+a    );
+            System.out.println(a+"     "+ a+            "  "+a+" "+a+" "+a+   "  "+a +" "+a+" "+a +" "+a  +"  "+a+" " +a+" "+a+" "+a+"   "+a+" "+a     );
+
+        }
+    }
+
