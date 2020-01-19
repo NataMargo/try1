edefd4292370810a74a77d01b7f99b31da18a896
Nataliia Chaika <larusbiz@gmail.com>
2020-01-19 12:51:58 +0200

    Lesson4

--- JavaLesson4_2/src/com/company/Main.java
+++ JavaLesson4_2/src/com/company/Main.java
@@ -5,20 +5,21 @@ import java.util.Scanner;
 import static jdk.nashorn.internal.objects.NativeString.substring;
 
 public class Main {
-    static String convertToBinory(int number) {
+    static String convertToBinary(int number) {
         String result = Integer.toBinaryString(number);
         return result;
         }
-
 
     public static void main(String[] args) {
         var Scanner = new Scanner(System.in);
         System.out.println("Enter integer number");
         int number = Scanner.nextInt();
-        String result = convertToBinory(number);
+        String result = convertToBinary(number);
         System.out.print(result);
     }
 }
+
+
 /*Напишите программу на языке Java, которая вводит целое число,
 преобразует его в двоичное представление, сохраняя как строку, и выводит в
 консоль результат. Преобразование выполняется отдельным методом.*/
